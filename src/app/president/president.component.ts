0640e858395def2e184d73d91dc4e94c7d35dea2
mignamayeSobhe <niebores@gmail.com>
2020-08-14 00:52:44 +0200

    Add president presentation

--- src/app/president/president.component.ts
+++ src/app/president/president.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-president',
   templateUrl: './president.component.html',
-  styleUrls: ['./president.component.css'],
+  styleUrls: ['./president.component.scss'],
 })
 export class PresidentComponent implements OnInit {
+  isLinear = false;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
